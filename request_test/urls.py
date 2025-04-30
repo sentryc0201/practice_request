6daa8fec906b256bde0800dc578657d7fd913d18
sentryc0201 <yc020102@gmail.com>
2025-04-30 20:17:00 +0900

    update all code

---
+++ request_test/urls.py
@@ -0,0 +1,25 @@
+"""
+URL configuration for myproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from .views import request_info_view
+from .views import file_upload_view
+
+urlpatterns = [
+    path('request-info/', request_info_view, name='request_info'),
+    path('upload/', file_upload_view, name='upload_file'),
+]
